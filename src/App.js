3f9e6fa7088d611cf7075a503538c6d4126e831c
Yurii Bodnar <yuriibodnar@MacBook-Pro-Yurii.local>
2022-03-24 13:38:47 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 function App() {
 
   return (
-    <div className='questionnare-box'>
-      <div className='content-wrapper'>
-        <div className='process'>
-          <div className='process-load'>
+   <div className='hbz-widget-box'>
+      <div className='questionnare-box'>
+        <div className='content-wrapper'>
+          <div className='process'>
+            <div className='process-load'>
+            </div>
           </div>
+          <span>
+            We are going to ask you a few question
+            s to more accurately select courses for you
+          </span>
         </div>
-        <span>
-          We are going to ask you a few question
-          s to more accurately select courses for you
-        </span>
+        <button className='start-button'>START</button>
       </div>
-      <button className='start-button'>START</button>
     </div>
   );
 }
